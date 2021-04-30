9941c0a450bcdc74262daa383abe5c9526bbfc4a
zhihui.li <lizhihui02152@gmail.com>
2021-04-30 11:31:33 +0800

    - add default value for JSON decoder

---
+++ Asset/SourceryGenerated/AutoCodable.generated.swift
@@ -0,0 +1,112 @@
+// Generated using Sourcery 1.4.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+extension Credential {
+
+    enum CodingKeys: String, CodingKey {
+        case userToken
+        case pageSize
+        case userOrgName
+        case userCityName
+        case userOrgId
+        case userCityId
+    }
+
+    internal init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        userToken = (try? container.decode(String.self, forKey: .userToken)) ?? Credential.defaultUserToken
+        pageSize = (try? container.decode(String.self, forKey: .pageSize)) ?? Credential.defaultPageSize
+        userOrgName = (try? container.decode(String.self, forKey: .userOrgName)) ?? Credential.defaultUserOrgName
+        userCityName = (try? container.decode(String.self, forKey: .userCityName)) ?? Credential.defaultUserCityName
+        userOrgId = (try? container.decode(String.self, forKey: .userOrgId)) ?? Credential.defaultUserOrgId
+        userCityId = (try? container.decode(String.self, forKey: .userCityId)) ?? Credential.defaultUserCityId
+    }
+
+}
+
+extension Location {
+
+    enum CodingKeys: String, CodingKey {
+        case isCheck
+        case locationCode
+        case locationName
+        case assetLocationId
+    }
+
+    internal init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        isCheck = (try? container.decode(Bool.self, forKey: .isCheck)) ?? Location.defaultIsCheck
+        locationCode = (try? container.decode(String.self, forKey: .locationCode)) ?? Location.defaultLocationCode
+        locationName = (try? container.decode(String.self, forKey: .locationName)) ?? Location.defaultLocationName
+        assetLocationId = (try? container.decode(String.self, forKey: .assetLocationId)) ?? Location.defaultAssetLocationId
+    }
+
+}
+
+extension LocationDetail {
+
+    enum CodingKeys: String, CodingKey {
+        case strLongitude
+        case locationClass
+        case regionIdDept
+        case locationOwner
+        case isCheck
+        case regionIdCompany
+        case province
+        case road
+        case regionIdManage
+        case enabledFlag
+        case locationOwnerName
+        case latitude
+        case city
+        case village
+        case locationId
+        case town
+        case locationCode
+        case strLatitude
+        case locationType
+        case longitude
+        case deptName
+        case companyName
+        case locationClassName
+        case county
+        case locationTypeName
+        case effectiveDate
+        case locationName
+    }
+
+    internal init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        strLongitude = (try? container.decode(String.self, forKey: .strLongitude)) ?? LocationDetail.defaultStrLongitude
+        locationClass = (try? container.decode(String.self, forKey: .locationClass)) ?? LocationDetail.defaultLocationClass
+        regionIdDept = (try? container.decode(String.self, forKey: .regionIdDept)) ?? LocationDetail.defaultRegionIdDept
+        locationOwner = (try? container.decode(String.self, forKey: .locationOwner)) ?? LocationDetail.defaultLocationOwner
+        isCheck = (try? container.decode(Bool.self, forKey: .isCheck)) ?? LocationDetail.defaultIsCheck
+        regionIdCompany = (try? container.decode(String.self, forKey: .regionIdCompany)) ?? LocationDetail.defaultRegionIdCompany
+        province = (try? container.decode(String.self, forKey: .province)) ?? LocationDetail.defaultProvince
+        road = (try? container.decode(String.self, forKey: .road)) ?? LocationDetail.defaultRoad
+        regionIdManage = (try? container.decode(String.self, forKey: .regionIdManage)) ?? LocationDetail.defaultRegionIdManage
+        enabledFlag = (try? container.decode(String.self, forKey: .enabledFlag)) ?? LocationDetail.defaultEnabledFlag
+        locationOwnerName = (try? container.decode(String.self, forKey: .locationOwnerName)) ?? LocationDetail.defaultLocationOwnerName
+        latitude = (try? container.decode(CLLocationDegrees.self, forKey: .latitude)) ?? LocationDetail.defaultLatitude
+        city = (try? container.decode(String.self, forKey: .city)) ?? LocationDetail.defaultCity
+        village = (try? container.decode(String.self, forKey: .village)) ?? LocationDetail.defaultVillage
+        locationId = (try? container.decode(String.self, forKey: .locationId)) ?? LocationDetail.defaultLocationId
+        town = (try? container.decode(String.self, forKey: .town)) ?? LocationDetail.defaultTown
+        locationCode = (try? container.decode(String.self, forKey: .locationCode)) ?? LocationDetail.defaultLocationCode
+        strLatitude = (try? container.decode(String.self, forKey: .strLatitude)) ?? LocationDetail.defaultStrLatitude
+        locationType = (try? container.decode(String.self, forKey: .locationType)) ?? LocationDetail.defaultLocationType
+        longitude = (try? container.decode(CLLocationDegrees.self, forKey: .longitude)) ?? LocationDetail.defaultLongitude
+        deptName = (try? container.decode(String.self, forKey: .deptName)) ?? LocationDetail.defaultDeptName
+        companyName = (try? container.decode(String.self, forKey: .companyName)) ?? LocationDetail.defaultCompanyName
+        locationClassName = (try? container.decode(String.self, forKey: .locationClassName)) ?? LocationDetail.defaultLocationClassName
+        county = (try? container.decode(String.self, forKey: .county)) ?? LocationDetail.defaultCounty
+        locationTypeName = (try? container.decode(String.self, forKey: .locationTypeName)) ?? LocationDetail.defaultLocationTypeName
+        effectiveDate = (try? container.decode(String.self, forKey: .effectiveDate)) ?? LocationDetail.defaultEffectiveDate
+        locationName = (try? container.decode(String.self, forKey: .locationName)) ?? LocationDetail.defaultLocationName
+    }
+
+}
